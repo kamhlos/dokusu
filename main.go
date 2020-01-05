75e19b2550b52ee74d71d2f8b24eb87b82f1dde5
Manolis Kamilakis <manos@automa.gr>
2020-01-05 11:30:45 +0200

    how to print cell content now?

--- main.go
+++ main.go
@@ -2,118 +2,270 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
+	//"math/rand"
+	//"time"
 )
 
-type Cell struct {
-	num   int
-	gen   bool // auto-generated numbers have a default color
-	valid bool // those have a red or green color
+type Number struct {
+	num int
+	row int
+	col int
 }
 
-var numpos [8]Cell // where else this number is found
+type Cell struct {
+	num      int
+	row      int
+	col      int
+	gen      bool  // auto-generated numbers have a default color
+	valid    bool  // those have a red or green color
+	selected bool  // selected cells have a yellow color
+	marks    []int // other num candidates (if num = 0)
+}
 
-var cells = [9][9]Cell{
-	[9]Cell{
-		Cell{num: 8, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 5, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 9, gen: true},
-		Cell{num: 7, gen: true},
-		Cell{num: 4, gen: true},
-		Cell{num: 0, gen: true},
-	},
-	[9]Cell{
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 3, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 8, gen: true},
-		Cell{num: 6, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 9, gen: true},
-		Cell{num: 0, gen: true},
-	},
-	[9]Cell{
-		Cell{num: 0, gen: true},
-		Cell{num: 9, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 4, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 2, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 6, gen: true},
-		Cell{num: 0, gen: true},
-	},
-	[9]Cell{
-		Cell{num: 0, gen: true},
-		Cell{num: 2, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 5, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 3, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-	},
-	[9]Cell{
-		Cell{num: 0, gen: true},
-		Cell{num: 5, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 6, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 9, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 4, gen: true},
-	},
-	[9]Cell{
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 4, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 8, gen: true},
-		Cell{num: 6, gen: true},
-		Cell{num: 2, gen: true},
-		Cell{num: 0, gen: true},
-	},
-	[9]Cell{
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 2, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 3, gen: true},
-	},
-	[9]Cell{
-		Cell{num: 4, gen: true},
-		Cell{num: 3, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 6, gen: true},
-		Cell{num: 1, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 5, gen: true},
-		Cell{num: 0, gen: true},
-	},
-	[9]Cell{
-		Cell{num: 9, gen: true},
-		Cell{num: 1, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 8, gen: true},
-		Cell{num: 0, gen: true},
-		Cell{num: 5, gen: true},
-		Cell{num: 4, gen: true},
-		Cell{num: 7, gen: true},
-		Cell{num: 6, gen: true},
-	},
+// map of number location in cells
+var positions map[int][]string
+
+var board = [81]Cell{
+	Cell{row: 0, col: 0, gen: true, num: 8},
+	Cell{row: 0, col: 1, gen: true, num: 0},
+	Cell{row: 0, col: 2, gen: true, num: 5},
+	Cell{row: 0, col: 3, gen: true, num: 0},
+	Cell{row: 0, col: 4, gen: true, num: 0},
+	Cell{row: 0, col: 5, gen: true, num: 9},
+	Cell{row: 0, col: 6, gen: true, num: 7},
+	Cell{row: 0, col: 7, gen: true, num: 4},
+	Cell{row: 0, col: 8, gen: true, num: 0},
+	Cell{row: 1, col: 0, gen: true, num: 0},
+	Cell{row: 1, col: 1, gen: true, num: 0},
+	Cell{row: 1, col: 2, gen: true, num: 3},
+	Cell{row: 1, col: 3, gen: true, num: 0},
+	Cell{row: 1, col: 4, gen: true, num: 8},
+	Cell{row: 1, col: 5, gen: true, num: 6},
+	Cell{row: 1, col: 6, gen: true, num: 0},
+	Cell{row: 1, col: 7, gen: true, num: 9},
+	Cell{row: 1, col: 8, gen: true, num: 0},
+	Cell{row: 2, col: 0, gen: true, num: 0},
+	Cell{row: 2, col: 1, gen: true, num: 9},
+	Cell{row: 2, col: 2, gen: true, num: 0},
+	Cell{row: 2, col: 3, gen: true, num: 4},
+	Cell{row: 2, col: 4, gen: true, num: 0},
+	Cell{row: 2, col: 5, gen: true, num: 2},
+	Cell{row: 2, col: 6, gen: true, num: 0},
+	Cell{row: 2, col: 7, gen: true, num: 6},
+	Cell{row: 2, col: 8, gen: true, num: 0},
+	Cell{row: 3, col: 0, gen: true, num: 0},
+	Cell{row: 3, col: 1, gen: true, num: 2},
+	Cell{row: 3, col: 2, gen: true, num: 0},
+	Cell{row: 3, col: 3, gen: true, num: 5},
+	Cell{row: 3, col: 4, gen: true, num: 0},
+	Cell{row: 3, col: 5, gen: true, num: 3},
+	Cell{row: 3, col: 6, gen: true, num: 0},
+	Cell{row: 3, col: 7, gen: true, num: 0},
+	Cell{row: 3, col: 8, gen: true, num: 0},
+	Cell{row: 4, col: 0, gen: true, num: 0},
+	Cell{row: 4, col: 1, gen: true, num: 5},
+	Cell{row: 4, col: 2, gen: true, num: 0},
+	Cell{row: 4, col: 3, gen: true, num: 6},
+	Cell{row: 4, col: 4, gen: true, num: 0},
+	Cell{row: 4, col: 5, gen: true, num: 0},
+	Cell{row: 4, col: 6, gen: true, num: 9},
+	Cell{row: 4, col: 7, gen: true, num: 0},
+	Cell{row: 4, col: 8, gen: true, num: 4},
+	Cell{row: 5, col: 0, gen: true, num: 0},
+	Cell{row: 5, col: 1, gen: true, num: 0},
+	Cell{row: 5, col: 2, gen: true, num: 4},
+	Cell{row: 5, col: 3, gen: true, num: 0},
+	Cell{row: 5, col: 4, gen: true, num: 0},
+	Cell{row: 5, col: 5, gen: true, num: 8},
+	Cell{row: 5, col: 6, gen: true, num: 6},
+	Cell{row: 5, col: 7, gen: true, num: 2},
+	Cell{row: 5, col: 8, gen: true, num: 0},
+	Cell{row: 6, col: 0, gen: true, num: 0},
+	Cell{row: 6, col: 1, gen: true, num: 0},
+	Cell{row: 6, col: 2, gen: true, num: 0},
+	Cell{row: 6, col: 3, gen: true, num: 0},
+	Cell{row: 6, col: 4, gen: true, num: 0},
+	Cell{row: 6, col: 5, gen: true, num: 0},
+	Cell{row: 6, col: 6, gen: true, num: 2},
+	Cell{row: 6, col: 7, gen: true, num: 0},
+	Cell{row: 6, col: 8, gen: true, num: 3},
+	Cell{row: 7, col: 0, gen: true, num: 4},
+	Cell{row: 7, col: 1, gen: true, num: 3},
+	Cell{row: 7, col: 2, gen: true, num: 0},
+	Cell{row: 7, col: 3, gen: true, num: 0},
+	Cell{row: 7, col: 4, gen: true, num: 6},
+	Cell{row: 7, col: 5, gen: true, num: 1},
+	Cell{row: 7, col: 6, gen: true, num: 0},
+	Cell{row: 7, col: 7, gen: true, num: 5},
+	Cell{row: 7, col: 8, gen: true, num: 0},
+	Cell{row: 8, col: 0, gen: true, num: 9},
+	Cell{row: 8, col: 1, gen: true, num: 1},
+	Cell{row: 8, col: 2, gen: true, num: 0},
+	Cell{row: 8, col: 3, gen: true, num: 8},
+	Cell{row: 8, col: 4, gen: true, num: 0},
+	Cell{row: 8, col: 5, gen: true, num: 5},
+	Cell{row: 8, col: 6, gen: true, num: 4},
+	Cell{row: 8, col: 7, gen: true, num: 7},
+	Cell{row: 8, col: 8, gen: true, num: 6},
+}
+
+// var cells = [9][9]Cell{
+// 	[9]Cell{
+// 		Cell{num: 8, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 5, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 9, gen: true},
+// 		Cell{num: 7, gen: true},
+// 		Cell{num: 4, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 3, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 8, gen: true},
+// 		Cell{num: 6, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 9, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 9, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 4, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 2, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 6, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 2, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 5, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 3, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 5, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 6, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 9, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 4, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 4, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 8, gen: true},
+// 		Cell{num: 6, gen: true},
+// 		Cell{num: 2, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 2, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 3, gen: true},
+// 		Cell{num: 4, gen: true},
+// 		Cell{num: 3, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 6, gen: true},
+// 		Cell{num: 1, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 5, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 9, gen: true},
+// 		Cell{num: 1, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 8, gen: true},
+// 		Cell{num: 0, gen: true},
+// 		Cell{num: 5, gen: true},
+// 		Cell{num: 4, gen: true},
+// 		Cell{num: 7, gen: true},
+// 		Cell{num: 6, gen: true},
+// 	},
+// }
+
+// func mapNumberPositions() error {
+
+// 	positions = make(map[int][]string)
+
+// 	for i := 0; i < 9; i++ {
+// 		for j := 0; j < 9; j++ {
+// 			// iterate over all cells
+// 			// map existing numbers to locations
+
+// 			positions[cells[i][j].num] = append(positions[cells[i][j].num], fmt.Sprintf("%d:", i)+fmt.Sprintf("%d", j))
+
+// 		}
+// 	}
+
+// 	// above loop produces a map of number positions:
+	
+// 		map[int][]string{
+// 			0:[]string{
+// 				"0:1", "0:3", "0:4", "0:8", "1:0", "1:1", "1:3", "1:6", "1:8", "2:0", "2:2", "2:4", "2:6", "2:8", "3:0", "3:2", "3:4", "3:6", "3:7", "3:8", "4:0", "4:2", "4:4", "4:5", "4:7", "5:0", "5:1", "5:3", "5:4", "5:8", "6:0", "6:1", "6:2", "6:3", "6:4", "6:5", "6:7", "7:2", "7:3", "7:6", "7:8", "8:2", "8:4"
+// 			},
+// 			1:[]string{
+// 				"7:5", "8:1"
+// 			},
+// 			2:[]string{
+// 				"2:5", "3:1", "5:7", "6:6"
+// 			},
+// 			3:[]string{
+// 				"1:2", "3:5", "6:8", "7:1"
+// 			},
+// 			4:[]string{
+// 				"0:7", "2:3", "4:8", "5:2", "7:0", "8:6"
+// 			},
+// 			5:[]string{
+// 				"0:2", "3:3", "4:1", "7:7", "8:5"
+// 			},
+// 			6:[]string{
+// 				"1:5", "2:7", "4:3", "5:6", "7:4", "8:8"
+// 			},
+// 			7:[]string{
+// 				"0:6", "8:7"
+// 			},
+// 			8:[]string{
+// 				"0:0", "1:4", "5:5", "8:3"
+// 			},
+// 			9:[]string{"0:5", "1:7", "2:1", "4:6", "8:0"
+// 			}
+// 		}
+	
+
+// 	return nil
+// }
+
+func validatePositions() error {
+
+	// TODO:
+	// validate current number positions
+	// return error if puzzle is not valid
+	// RULES for validation:
+	// each row/column/9cell box has no more than one copy of each number of 1 to 9
+	for i := 1; i < 10; i++ {
+
+	}
+
+	return nil
+}
+
+func (c Cell) highlight() error {
+
+	c.selected = true
+
+	return nil
 }
 
 func (c Cell) Content() string {
@@ -125,7 +277,11 @@ func (c Cell) Content() string {
 
 	if !c.gen {
 		if c.valid {
-			color = "92"
+			if c.selected {
+				color = "84"
+			} else {
+				color = "92"
+			}
 		} else {
 			color = "31"
 		}
@@ -146,11 +302,26 @@ func main() {
 	// i.e generate a random puzzle to solve
 	// randomCells()
 
-	printBoard()
+	// printBoard()
 
-	printBoard()
+	// printBoard()
 
-	fmt.Printf("\n\n")
+	// fmt.Printf("\n\n")
+}
+
+// difficulty measured by the count of 0's;
+// > 30 considered easy, < 25 probably hard
+func difficulty() string {
+
+	// 	if len(positions[0]) > 30 {
+	// 		return "easy"
+	// 	}
+
+	// 	if len(positions[0]) < 25 {
+	// 		return "hard"
+	// 	}
+
+	// 	return fmt.Sprintf("count of 0's: %d", len(positions[0]))
 }
 
 // cross-hatching method:
@@ -162,31 +333,27 @@ func main() {
 // find all row and columns which that digit is missing
 func crosshatch() {
 
-	location := make(map[int][]string)
+	// location := make(map[int][]string)
 
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
-			// iterate over all cells
-			// map existing numbers to locations
-			location[cells[i][j].num] = append(location[cells[i][j].num], fmt.Sprintf("row:%d", i)+fmt.Sprintf("col:%d", j))
+	// for i := 0; i < 9; i++ {
+	// 	for j := 0; j < 9; j++ {
+	// 		// iterate over all cells
+	// 		// map existing numbers to locations
+	// 		location[cells[i][j].num] = append(location[cells[i][j].num], fmt.Sprintf("row:%d", i)+fmt.Sprintf("col:%d", j))
 
-		}
-	}
-}
-
-func highlight(line int, column int, num int) {
-	// cells[line][column] = printCell(false, true, num)
+	// 	}
+	// }
 }
 
 func randomCells() {
 
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	// r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
-			cells[i][j].num = r.Intn(9) + 1
-		}
-	}
+	// for i := 0; i < 9; i++ {
+	// 	for j := 0; j < 9; j++ {
+	// 		cells[i][j].num = r.Intn(9) + 1
+	// 	}
+	// }
 
 	// for i := 0; i < 9; i++ {
 	// 	ec := r.Intn(10)
@@ -195,95 +362,99 @@ func randomCells() {
 	// }
 }
 
-func validateCell(cell [9][9]string) bool {
-	// TODO:
-	// logic to validate each cell
+// func validateCell(cell [9][9]string) bool {
+// 	// TODO:
+// 	// logic to validate each cell
 
-	return false
-}
+// 	return false
+// }
 
-// prints the board with the cells contents if any
+// prints the board with the cells contents if num not zero
 func printBoard() {
 
-	// START first line of boxes
+	// START first row of boxes
 
 	// upper border
 	fmt.Printf("\u250F\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u2533\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u2533\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u252F\u2501\u2501\u2501\u2513\n")
 
-	// first line of numbers
-	printNumberLine(cells[0])
+	// first row of numbers
+	printNumberRow(0)
 
-	// first middle line
+	// first middle row
 	fmt.Printf("\u2520\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2542\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2542\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2528\n")
 
-	// second line of numbers
-	printNumberLine(cells[1])
+	// second row of numbers
+	printNumberRow(1)
 
-	// second middle line
+	// second middle row
 	fmt.Printf("\u2520\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2542\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2542\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2528\n")
 
-	// third line of numbers
-	printNumberLine(cells[2])
+	// third row of numbers
+	printNumberRow(2)
 
 	// lower border
 	fmt.Printf("\u2523\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u252B\n")
 
-	// END first line of boxes
+	// END first row of boxes
 
 	// REPEAT
-	// START second line of boxes (no border)
+	// START second row of boxes (no border)
 
-	// first line of numbers
-	printNumberLine(cells[3])
+	// first row of numbers
+	printNumberRow(3)
 
-	// first middle line
+	// first middle row
 	fmt.Printf("\u2520\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2542\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2542\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2528\n")
 
-	// second line of numbers
-	printNumberLine(cells[4])
+	// second row of numbers
+	printNumberRow(4)
 
-	// second middle line
+	// second middle row
 	fmt.Printf("\u2520\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2542\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2542\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2528\n")
 
-	// third line of numbers
-	printNumberLine(cells[5])
+	// third row of numbers
+	printNumberRow(5)
 
 	// lower border
 	fmt.Printf("\u2523\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u254B\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u253F\u2501\u2501\u2501\u252B\n")
 
-	// END second line of boxes
+	// END second row of boxes
 
 	// REPEAT
-	// START third line of boxes (no border)
+	// START third row of boxes (no border)
 
-	// first line of numbers
-	printNumberLine(cells[6])
+	// first row of numbers
+	printNumberRow(6)
 
-	// first middle line
+	// first middle row
 	fmt.Printf("\u2520\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2542\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2542\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2528\n")
 
-	// second line of numbers
-	printNumberLine(cells[7])
+	// second row of numbers
+	printNumberRow(7)
 
-	// second middle line
+	// second middle row
 	fmt.Printf("\u2520\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2542\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2542\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u253C\u2500\u2500\u2500\u2528\n")
 
-	// third line of numbers
-	printNumberLine(cells[8])
+	// third row of numbers
+	printNumberRow(8])
 
 	// lower border
 	fmt.Printf("\u2517\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u253B\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u253B\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u2537\u2501\u2501\u2501\u251B\n")
 
-	// END third line of boxes
+	// END third row of boxes
 
 	fmt.Printf("\n\n")
 }
 
-func printNumberLine(cells [9]Cell) {
+func printNumberRow(n int) {
 
-	// replace 2502 with 250A or 2506 for vertical lines
+	numtoprint = make(map[int]string)
+	// replace 2502 with 250A or 2506 for vertical lines as cells separators
+	for i := 0; i < 9; i++ {
+		numtoprint[i] = Cell
+	}
 
-	fmt.Printf("\u2503 %s \u2502 %s \u2502 %s \u2503", cells[0].Content(), cells[1].Content(), cells[2].Content())
+	fmt.Printf("\u2503 %s \u2502 %s \u2502 %s \u2503", Cell., cells[1].Content(), cells[2].Content())
 	fmt.Printf(" %s \u2502 %s \u2502 %s \u2503", cells[3].Content(), cells[4].Content(), cells[5].Content())
 	fmt.Printf(" %s \u2502 %s \u2502 %s \u2503\n", cells[6].Content(), cells[7].Content(), cells[8].Content())
 }
--- main_test.go
+++ main_test.go
@@ -5,33 +5,46 @@ import (
 	"testing"
 )
 
-// func TestPrintBoard(t *testing.T) {
-// 	printBoard(cells)
-// }
+func TestPrintBoard(t *testing.T) {
+
+	err := mapNumberPositions()
+
+	if err != nil {
+		fmt.Println("puzzle is invalid")
+	} else {
+		fmt.Println("Difficulty: ", difficulty())
+		fmt.Println("0's count: ", len(positions[0]))
+		printBoard()
+	}
+
+}
 
 func TestRandomCells(t *testing.T) {
-
-	location := make(map[int][]string)
 
 	// var locations []map[int]string
 
 	// testing cross-hatch method
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
-			// iterate over all cells
-			// map existing numbers to locations
-			location[cells[i][j].num] = append(location[cells[i][j].num], fmt.Sprintf("row:%d", i)+fmt.Sprintf("col:%d", j))
-
-		}
-	}
 
 	// debug
-	fmt.Printf("------------------------ DEBUG -------------------------\n")
-	fmt.Printf("%#v\n", location[8])
-	fmt.Printf("------------------------ DEBUG -------------------------\n")
+	// fmt.Printf("------------------------ DEBUG -------------------------\n")
+	// fmt.Printf("%#v\n", positions)
+	// fmt.Printf("------------------------ DEBUG -------------------------\n")
 
 }
 
 func TestHighlightRow(t *testing.T) {
 
+	// for _, v := range positions[0] {
+	// 	fmt.Println("0 is found in ", v)
+	// }
+}
+
+// only used to generate board code
+func TestPrintCellVar(t *testing.T) {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			fmt.Printf("[9]Cell{row: %d, col: %d, gen: true, num: }\n", i, j)
+		}
+
+	}
 }
